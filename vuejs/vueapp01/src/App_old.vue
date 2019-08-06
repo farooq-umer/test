ae0d791d9b511f8ce9151e26f5cc11f98e7aba90
Deri Ohuiginn <deriohig@gmail.com>
2019-08-06 14:14:36 +0100

    adding the vue project

---
+++ vuejs/vueapp01/src/App_old.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="app">
+    div(
+      v-for="user in users"
+      :key="users.id"
+    )
+      h1 {{ user.name }}
+      p {{ user.email }}
+    button(@click="fetchUsers") Click me!
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      users: []
+    }
+  },
+  methods: {
+    fetchUsers: function () {
+      const baseURI = 'https://jsonplaceholder.typicode.com/users'
+      this.$http.get(baseURI)
+      .then((result) => {
+        this.users = result.data
+      })
+    }
+  }
+}
+<script>
